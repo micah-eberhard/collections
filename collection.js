93eb46c8612a90b0bb760ef348a6912a006ceafd
micah-eberhard <micah.eberhard@gmail.com>
2015-12-02 09:40:42 -0700

    Added Dec02 warmup, array manipulations

--- collection.js
+++ collection.js
@@ -144,6 +144,74 @@ function nov30()
   timerApp();
 }
 
+/***************************
+Dec 2 Warmup
+Varrious array manipulations:
+Intersection, Union, Zip
+****************************/
+function dec2()
+{
+  console.log("\n\n-----Dec02-----\nVarrious array manipulations: Intersection, Union, Zip\n");
+  function intersect(arr1, arr2)
+  {
+    var arr3 = [];
+    for (var item in arr1)
+    {
+      for(var stuff in arr2)
+      {
+        if(arr2[stuff] === arr1[item])
+        {
+          arr3.push(arr1[item]);
+        }
+      }
+    }
+    return arr3;
+  }
+  function union(arr1, arr2)
+  {
+    var arr3 = [];
+    for(var item in arr1)
+    {
+      arr3.push(arr1[item]);
+    }
+    for(var item in arr2)
+    {
+      arr3.push(arr2[item]);
+    }
+    return arr3;
+  }
+  function zip(arr1, arr2)
+  {
+    var arr3 = [];
+    var len = 0;
+    if(arr1.length > arr2.length)
+    {
+      len = arr1.length;
+    }
+    else
+    {
+      len = arr2.length;
+    }
+
+    for(var i = 0; i < len; i++)
+    {
+      arr3.push(arr1[i]);
+      arr3.push(arr2[i]);
+    }
+    return arr3;
+  }
+
+
+
+
+  var arrOne = [1,2,3];
+  var arrTwo = [1,3,5];
+  var arrThree = [4,5,6];
+
+  console.log(intersect(arrOne, arrTwo));
+  console.log(union(arrOne, arrThree));
+  console.log(zip(arrOne, arrThree));
+}
 
 /***************************
 Functions
@@ -152,3 +220,4 @@ Functions
 nov23();
 nov24();
 nov30();
+dec2();
